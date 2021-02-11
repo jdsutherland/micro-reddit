0e30db7667638db4dd3e4e0443d1159b8f1e2b0a
Jeff Sutherland <jeffdsuth@gmail.com>
2021-02-10 19:05:33 -0700

    Fix tests (revert previous)
    The previous FactoryBot issue has inexplicably reverted itself.

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -6,6 +6,11 @@ describe Post, type: :model do
   context 'validations' do
     it { should validate_presence_of(:title) }
     it { should validate_presence_of(:url) }
+  end
+
+  context 'uniqueness' do
+    before { create(:post) }
+
     it { should validate_uniqueness_of(:title) }
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -8,7 +8,11 @@ describe User do
     it { should validate_presence_of(:email) }
     it { should validate_presence_of(:encrypted_password) }
 
-    it { should validate_uniqueness_of(:username) }
-    it { should validate_uniqueness_of(:email) }
+    context 'uniqueness' do
+      before { create(:user) }
+
+      it { should validate_uniqueness_of(:username) }
+      it { should validate_uniqueness_of(:email) }
+    end
   end
 end
